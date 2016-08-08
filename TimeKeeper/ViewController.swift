fd0755973904567612453425aacbffeadee5b8df
neliSun <nelson.chow91.nc@gmail.com>
2016-08-08 11:36:28 -0700

    Initial Commit

---
+++ TimeKeeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeKeeper
+//
+//  Created by Nelson Chow on 2016-08-08.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
